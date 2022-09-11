29603ef911a873755caad13bca0c6f7289c5f59d
Megh2507 <megha25072001@gmail.com>
2022-09-12 00:55:37 +0530

    adding cart section order section payment section

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import HotAccessoriesMenu from './components/HotAccessoriesMenu';
 import HotAccessories from './components/HotAccessories';
 import Footer from './components/Footer';
 import NavOption from './components/NavOption';
+import Cart from './components/Cart';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/cart" element={<Cart />} />
           <Route exact path='/music' element={<HotAccessories music={data.hotAccessories.music} musicCover={data.hotAccessoriesCover.music} />} />
           <Route exact path='/device' element={<HotAccessories music={data.hotAccessories.smartDevice} musicCover={data.hotAccessoriesCover.smartDevice} />} />
           <Route exact path='/home' element={<HotAccessories music={data.hotAccessories.home} musicCover={data.hotAccessoriesCover.home} />} />
---
+++ src/components/OrderInfo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./cart.css"
+function OrderInfo() {
+  return (
+    <div className='os'>
+ <input type="text" placeholder='Enter your full name' />
+ <input type="number" placeholder='Enter your phone number' />
+ <input type="text" placeholder='Address line 1' />
+ <input type="text" placeholder='Address line 2' />
+ <button className='bbtn'>Buy Now</button>
+
+    </div>
+  )
+}
+
+export default OrderInfo
